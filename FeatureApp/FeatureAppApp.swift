014d88da8eb983a7bc21619bafcd18d51f19c611
Philipp Kathöfer <philippkathoefr@gmail.com>
2023-02-25 16:38:29 +0100

    Initial Commit

---
+++ FeatureApp/FeatureAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FeatureAppApp.swift
+//  FeatureApp
+//
+//  Created by Philipp Kathöfer on 25.02.23.
+//
+
+import SwiftUI
+
+@main
+struct FeatureAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
